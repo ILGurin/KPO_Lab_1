203492fb5851c020fb7086d66aea02c5552c3024
ILGurin <126956586+ILGurin@users.noreply.github.com>
2023-03-06 10:45:16 +0300

    Add file Lab 1 (1,2)

---
+++ KPO Lab_1 {1}.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int n, m, a;
+    unsigned long long result, mp, np;
+    cin >> n;
+    cin >> m;
+    cin >> a;
+    if (n % a != 0) {
+        np = ceil(((double)n / (double)a));
+    }
+    else {
+        np = n / a;
+    }
+    if (m % a != 0) {
+        mp = ceil(((double)m / (double)a));
+    }
+    else {
+        mp = m / a;
+    }
+    result = mp * np;
+    cout << result << endl;
+}
